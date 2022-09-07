c0bdca4cc02b5fad2106f45ba6380096507ed689
Max Brosnahan <maximilianbrosnahan@gmail.com>
2022-09-07 22:43:21 +0200

    Add formatted message using one cabin as an example

--- src/discord/discord.service.ts
+++ src/discord/discord.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { Client, GatewayIntentBits } from 'discord.js';
+import { Client, EmbedBuilder, GatewayIntentBits } from 'discord.js';
 import * as cron from 'node-cron';
 
 @Injectable()
@@ -15,7 +15,9 @@ export class DiscordService {
     this.client.on('ready', () => {
       console.log(`Logged in as ${this.client.user?.tag}!`);
       console.log(this.client.channels.cache.entries());
-      this.task = cron.schedule('* * * * *', this.onMinute);
+      if ('false' + 1 === 'true') {
+        this.task = cron.schedule('* * * * *', this.onMinute);
+      }
       this.onMinute();
     });
 
@@ -49,7 +51,50 @@ export class DiscordService {
     if (!channel) {
       return;
     }
-    //@ts-ignore
-    channel.send('hi there');
+    if (channel.isTextBased()) {
+      const embed = new EmbedBuilder()
+        .setColor(0xd82d20)
+        .setTitle('Dagens Hytta - Geitungen fyr')
+        .setURL('https://ut.no/hytte/10386/geitungen-fyr')
+        // .setAuthor({
+        //   name: 'Some name',
+        //   iconURL: 'https://i.imgur.com/AfFp7pu.png',
+        //   url: 'https://discord.js.org',
+        // })
+        .setDescription(
+          'Unik overnatting på øya utenfor Skudeneshavn på Karmøy, som huser Geitungen fyr, som eneste bolig. Haugesund Turistforening driver fyrvokterboligen som turisthytte på linje med andre hytter som foreningen disponerer. Hytta er åpen i perioden 1. mai til 30. september. ',
+        )
+        .setThumbnail(
+          'https://cdn.dnt.org/prod/sherpa/build/permanent/static/img/common/header-logo-part.png',
+        )
+        // .addFields(
+        //   { name: 'Regular field title', value: 'Some value here' },
+        //   { name: '\u200B', value: '\u200B' },
+        //   {
+        //     name: 'Inline field title',
+        //     value: 'Some value here',
+        //     inline: true,
+        //   },
+        //   {
+        //     name: 'Inline field title',
+        //     value: 'Some value here',
+        //     inline: true,
+        //   },
+        // )
+        // .addFields({
+        //   name: 'Inline field title',
+        //   value: 'Some value here',
+        //   inline: true,
+        // })
+        .setImage(
+          'https://res.cloudinary.com/ntb/image/upload/w_1280,q_80/v1/cabins/eidlrui481lox0ittb8s',
+        )
+        .setTimestamp();
+      // .setFooter({
+      //   text: 'Some footer text here',
+      //   iconURL: 'https://i.imgur.com/AfFp7pu.png',
+      // });
+      channel.send({ embeds: [embed] });
+    }
   }
 }
