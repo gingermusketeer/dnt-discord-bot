d5d7f461b1e1abf022ef4b09aa373fe3fe5aa693
Luisa <post@ansmeer.com>
2022-10-19 16:12:44 +0200

    refactor: clean up visbook api request parameters

--- src/visbook/visbook.api.ts
+++ src/visbook/visbook.api.ts
@@ -21,22 +21,7 @@ export class VisbookApi {
         accept: 'application/json, text/plain, */*',
         'accept-language': 'no,nb-NO',
         'cache-control': 'no-cache',
-        pragma: 'no-cache',
-        'sec-ch-ua':
-          '"Chromium";v="106", "Google Chrome";v="106", "Not;A=Brand";v="99"',
-        'sec-ch-ua-mobile': '?0',
-        'sec-ch-ua-platform': '"macOS"',
-        'sec-fetch-dest': 'empty',
-        'sec-fetch-mode': 'cors',
-        'sec-fetch-site': 'same-site',
-        'x-requested-with': 'XMLHttpRequest',
       },
-      referrer: 'https://reservations.visbook.com/',
-      referrerPolicy: 'strict-origin-when-cross-origin',
-      body: null,
-      method: 'GET',
-      mode: 'cors',
-      credentials: 'include',
     });
 
     return response.json();
