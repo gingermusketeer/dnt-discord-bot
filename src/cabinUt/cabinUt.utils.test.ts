31bafba596c07c9220c1d4f1ffd64b0711d4d632
Luisa <post@ansmeer.com>
2022-10-30 11:26:09 +0100

    test: include more relevant tests

--- src/cabinUt/cabinUt.utils.test.ts
+++ src/cabinUt/cabinUt.utils.test.ts
@@ -38,8 +38,16 @@ describe('Determine if booking URL points to a reservations page', () => {
     const url = 'https://reservations.visbook.com/6093';
     assert.deepEqual(isReservationsUrl(url), true);
   });
-  it('returns true if url does not contain reservations', () => {
+  it('returns false if url does not contain reservations', () => {
     const url = 'https://booking.visbook.com/no/6093';
+    assert.deepEqual(isReservationsUrl(url), false);
+  });
+  it('returns false for an email address', () => {
+    const url = 'reservations@dnt.hytte';
+    assert.deepEqual(isReservationsUrl(url), false);
+  });
+  it('returns false for text only', () => {
+    const url = 'For reservations call us at 123 456';
     assert.deepEqual(isReservationsUrl(url), false);
   });
 });
