a924be3802bb72dbc8c538c767bdb65e12e0d3fd
Luisa <post@ansmeer.com>
2022-10-31 13:53:17 +0100

    fix: add missing imports and tests

--- src/cabin/cabin.utils.test.ts
+++ src/cabin/cabin.utils.test.ts
@@ -1,5 +1,15 @@
 import * as assert from 'node:assert';
-import { dateIsValid } from './cabin.utils';
+import { describe, it } from 'node:test';
+import { dateIsValid, hasExceededTimeLimit } from './cabin.utils';
+
+describe('Checking time limit', () => {
+  it('returns false if time limit is not passed', () => {
+    assert.deepEqual(hasExceededTimeLimit(1000, 1001, 5), false);
+  });
+  it('returns true if time limit is passed', () => {
+    assert.deepEqual(hasExceededTimeLimit(1000, 2000, 5), true);
+  });
+});
 
 describe('Validating input as date string yyyy-mm-dd', () => {
   it('returns true for valid date string', () => {
