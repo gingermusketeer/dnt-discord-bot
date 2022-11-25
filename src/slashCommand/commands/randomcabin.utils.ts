3895e1ceb49a05f0c46fc7907ac3cbc75d52184f
Luisa <luisa.mohle@no.experis.com>
2022-11-25 14:17:12 +0100

    Make schema local file const

--- src/slashCommand/commands/randomcabin.utils.ts
+++ src/slashCommand/commands/randomcabin.utils.ts
@@ -3,29 +3,25 @@ import { startOfDay, addDays } from 'date-fns';
 
 export type BookingDates = { checkIn: Date; checkOut: Date };
 
+const schema = object({
+  checkIn: date()
+    .required('Check-in is required')
+    .min(startOfDay(new Date()), 'Check-in cannot be in the past'),
+  checkOut: date()
+    .when('checkIn', (checkIn, schema) => {
+      if (checkIn) {
+        return schema.min(
+          addDays(checkIn, 1),
+          'Check-out must be after check-in',
+        );
+      } else {
+        return schema;
+      }
+    })
+    .required('Check-out is required'),
+});
+
 export class BookingDatesSchema {
-  private schema;
-
-  constructor() {
-    this.schema = object({
-      checkIn: date()
-        .required('Check-in is required')
-        .min(startOfDay(new Date()), 'Check-in cannot be in the past'),
-      checkOut: date()
-        .when('checkIn', (checkIn, schema) => {
-          if (checkIn) {
-            return schema.min(
-              addDays(checkIn, 1),
-              'Check-out must be after check-in',
-            );
-          } else {
-            return schema;
-          }
-        })
-        .required('Check-out is required'),
-    });
-  }
-
   public async validate(
     checkIn: string | null,
     checkOut: string | null,
@@ -36,7 +32,7 @@ export class BookingDatesSchema {
     };
 
     try {
-      return await this.schema.validate(data, {
+      return await schema.validate(data, {
         abortEarly: false,
       });
     } catch (e) {
